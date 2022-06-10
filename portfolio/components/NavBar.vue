359eaf3d8424506eba46de4a1318b5ec3334678f
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-06-10 13:24:12 -0400

    Started

---
+++ portfolio/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="navbar">
+      <NuxtLink to="/">
+          <div class="logo">
+              Logo
+          </div>
+      </NuxtLink>
+      <div class="menu">
+          
+             <NuxtLink to="/">Projects</NuxtLink>
+             <NuxtLink to="/">Resume</NuxtLink>
+             <NuxtLink to="/">Contact</NuxtLink>
+        
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.menu{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    width: 50%;
+}
+
+
+</style>
---
+++ portfolio/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <NavBar />
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
