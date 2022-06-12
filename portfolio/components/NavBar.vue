9f422ee07485338651becc776c19a348966b9dea
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-06-12 18:38:44 -0400

    methods work

--- portfolio/components/NavBar.vue
+++ portfolio/components/NavBar.vue
@@ -1,41 +1,48 @@
 <template>
   <section class="navbar">
-      <NuxtLink to="/">
-          <div class="logo">
-              Logo
-          </div>
-      </NuxtLink>
-      <div class="menu">
-          
-             <NuxtLink to="/">Projects</NuxtLink>
-             <NuxtLink to="/">Resume</NuxtLink>
-             <NuxtLink to="/">Contact</NuxtLink>
-        
+    <NuxtLink to="/">
+      <div class="logo">
+        {{ name }}
       </div>
+    </NuxtLink>
+    <div class="menu">
+      <NuxtLink to="/">Projects</NuxtLink>
+      <NuxtLink to="/">Resume</NuxtLink>
+      <NuxtLink to="/">Contact</NuxtLink>
+    </div>
+    <button @click="test()">Submit</button>
   </section>
 </template>
 
-<script>
+<script lang="ts">
 export default {
+  data() {
+    return {
+      name: null,
+    };
+  },
 
-}
+  methods: {
+      test: function (){
+          this.name = "Johnson"
+      }
+  },
+};
 </script>
 
 <style scoped>
-.navbar{
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    align-items: center;
-    justify-content: space-around;
+.navbar {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content: space-around;
 }
 
-.menu{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    width: 50%;
+.menu {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 50%;
 }
-
-
 </style>
